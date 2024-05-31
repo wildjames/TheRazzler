a556f59effb8fa6dc1fff28a5d3457d91f5d7650
Jim Wild <james.wild@airelogic.com>
2024-05-31 19:27:50 +0100

    update temperature type

--- ai_interface/dataclasses.py
+++ ai_interface/dataclasses.py
@@ -8,12 +8,17 @@ class OpenAIConfig(BaseModel):
     quality_model: str = "gpt-3.5-turbo"
     vision_model: str = "gpt-4o"
     image_model: str = "dall-e-3"
-    chat_completion_kwargs: Dict[str, Union[str, int]] = Field(
+    chat_completion_kwargs: Dict[str, Union[str, int, float]] = Field(
         default_factory=dict
     )
-    vision_completion_kwargs: Dict[str, Union[str, int]] = Field(
+    vision_completion_kwargs: Dict[str, Union[str, int, float]] = Field(
         default_factory=dict
     )
-    image_generation_kwargs: Dict[str, Union[str, int]] = Field(
+    image_generation_kwargs: Dict[str, Union[str, int, float]] = Field(
         default_factory=dict
     )
+
+
+IMAGE_TOKENS = {
+    "256x256": 755,
+}
