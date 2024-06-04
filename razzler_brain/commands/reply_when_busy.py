46f9d49f4e21101e2dd1c89769320ee6066229e0
Jim Wild <james.wild@airelogic.com>
2024-06-04 09:57:44 +0100

    tweak bust chat detection

--- razzler_brain/commands/reply_when_busy.py
+++ razzler_brain/commands/reply_when_busy.py
@@ -1,4 +1,5 @@
 import json
+import random
 from logging import getLogger
 from typing import Optional
 
@@ -14,16 +15,27 @@ logger = getLogger(__name__)
 
 
 class ReplyWhenActiveChatCommandHandler(ReplyCommandHandler):
+    """Count the number of human messages since the Razzler last spoke: N.
+    The razzler generates a number between 0 and 1.
+    The threshold to then say something is calculated as:
+        (N - minimum_frequency) / (maximum_frequency - minimum_frequency)
+    In other words, the razzler starts off unlikely to say something, but
+    becomes more likely as the chat becomes more active. Once the maximum
+    frequency is reached, the razzler will always say something.
+    """
+
+    minumum_frequency = 5
+    maximum_frequency = 30
+
+    # Maximum time to scan, in seconds.
+    time_window = 60 * 60 * 6
+
     def can_handle(
         self,
         message: IncomingMessage,
         redis_connection: Optional[redis.Redis] = None,
         config: Optional[RazzlerBrainConfig] = None,
     ) -> bool:
-        """This command triggers if 10 messages are sent within 60 second of each
-        other, but only if no messages have a mention of the razzler and
-        no messages originate from the razzler.
-        """
 
         if not isinstance(message, IncomingMessage):
             return False
@@ -36,8 +48,7 @@ class ReplyWhenActiveChatCommandHandler(ReplyCommandHandler):
 
         # Each incoming message has a timestamp. Count how many messages were
         # sent within some number of seconds of the current message.
-        time_window = 60000  # ms
-        required_msgs = 10
+        time_window = self.time_window * 1000  # ms
 
         count = 0
         last_msg_time = message.envelope.timestamp
@@ -45,31 +56,43 @@ class ReplyWhenActiveChatCommandHandler(ReplyCommandHandler):
             try:
                 msg = IncomingMessage(**json.loads(record))
 
+                if msg.envelope.receiptMessage:
+                    # Don't count receipt messages
+                    continue
+
                 this_timestamp = msg.envelope.timestamp
                 if abs(this_timestamp - last_msg_time) < time_window:
                     count += 1
+                    logger.debug(
+                        f"Counting message: {msg.envelope.dataMessage.message}"
+                    )
             except pydantic.ValidationError:
-                # If the window contains a razzler message, then don't trigger
-                # We don't want to dominate the conversation
+                # If the window contains a razzler message, stop counting
                 try:
                     OutgoingMessage(**json.loads(record))
 
                     this_timestamp = msg.envelope.timestamp
                     if abs(this_timestamp - last_msg_time) < time_window:
-                        logger.info(
-                            "The chat is active, but I already replied at"
-                            f" {this_timestamp}"
+                        logger.debug(
+                            f"The razzler last replied at {this_timestamp}."
+                            " Stopping counting messages"
                         )
-                        return False
+                        break
 
                 except pydantic.ValidationError:
+                    # If we get here, it's an OutgoingReaction
                     continue
 
-                # If the record is not a valid IncomingMessage, skip it.
-                continue
-
         logger.info(
-            f"Found {count} messages within {time_window/1000} seconds"
+            f"Found {count} messages within {self.time_window} seconds"
         )
 
-        return count >= required_msgs
+        chance = (count - self.minumum_frequency) / (
+            self.maximum_frequency - self.minumum_frequency
+        )
+        # constrain the chance to be between 0 and 1
+        chance = max(0, min(1, chance))
+        logger.info(f"This gives the razzler a {chance:.3f} chance to reply")
+
+        r = random.random()
+        return r < chance
