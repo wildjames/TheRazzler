e9286dcbd8dbc5e46d9a36f2ec5fd482d5899f0c
Jim Wild <james.wild@airelogic.com>
2024-06-13 00:06:09 +0100

    CORS update

--- user_prefs_api/app.py
+++ user_prefs_api/app.py
@@ -32,7 +32,14 @@ db = get_mongo_db(config.mongodb)
 preferences_collection = initialize_preferences_collection(db)
 
 app = Flask(__name__)
-CORS(app, origins=["localhost", "wildjames.com", "127.0.0.1", "192.168.0.111"])
+CORS(
+    app,
+    origins=[
+        "localhost",
+        "127.0.0.1",
+        "https://razzler-web.wildjames.com",
+    ],
+)
 app.logger.setLevel(logging.INFO)
 
 
