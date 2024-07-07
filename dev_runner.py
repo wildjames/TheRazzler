8f09d9ad02537e232dcbd2166d3d9c7e189ef98b
James Wild <wild.james343@gmail.com>
2024-07-07 11:21:35 +0000

    Fix dev runner

--- dev_runner.py
+++ dev_runner.py
@@ -5,34 +5,36 @@ import sys
 from typing import List, Optional
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-from logging import getLogger, basicConfig, INFO
+from logging import getLogger, basicConfig, INFO, CRITICAL
+import fnmatch
 
 logger = getLogger(__name__)
 basicConfig(level=INFO)
 
+
 class ChangeHandler(FileSystemEventHandler):
     """Handles the file change events by restarting the application."""
 
-    def __init__(self, command: List[str], ignore_dirs: Optional[List[str]] = None):
+    def __init__(self, command: List[str], ignores: Optional[List[str]] = None):
         self.command = command
         self.env = os.environ.copy()
         self.process = subprocess.Popen(self.command, env=self.env)
-        self.ignore_dirs = ignore_dirs or []
+        self.ignores = ignores or []
 
         logger.info(f"Starting the managed process with command: {self.command}")
-        logger.info(f"Ignoring directories: {self.ignore_dirs}")
+        logger.info(f"Ignoring directories: {self.ignores}")
         logger.info(f"Process ID: {self.process.pid}")
 
     def on_any_event(self, event):
         if event.is_directory:
             return
 
-        # Check if the event path is in any of the ignored directories
-        for ignore_dir in self.ignore_dirs:
-            if event.src_path.startswith(ignore_dir):
+        # Check if the event path matches with any of the ignores patterns
+        for ignore_dir in self.ignores:
+            if fnmatch.fnmatch(event.src_path, ignore_dir):
                 return  # Ignore this event
 
-        if event.event_type in ['modified', 'created', 'moved']:
+        if event.event_type in ["modified", "created", "moved"]:
             logger.critical(f"Event: {event.event_type} on {event.src_path}")
             self.restart_process()
 
@@ -43,9 +45,12 @@ class ChangeHandler(FileSystemEventHandler):
             self.process.kill()
             self.process.wait()
         self.process = subprocess.Popen(self.command)
+        logger.info(f"Restarted the process with command: {self.command}")
+        logger.info(f"New Process ID: {self.process.pid}")
 
-def start_monitoring(path, command: List[str], ignore_dirs: Optional[List[str]] = None):
-    event_handler = ChangeHandler(command, ignore_dirs)
+
+def start_monitoring(path, command: List[str], ignores: Optional[List[str]] = None):
+    event_handler = ChangeHandler(command, ignores)
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
@@ -56,12 +61,13 @@ def start_monitoring(path, command: List[str], ignore_dirs: Optional[List[str]] 
         observer.stop()
     observer.join()
 
+
 if __name__ == "__main__":
     # Set the path to the project directory
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    path = sys.argv[1] if len(sys.argv) > 1 else "."
 
-    ignore_dirs = ['venv', '.git', '__pycache__', "data"]
-    ignore_dirs = [os.path.join(path, dir) for dir in ignore_dirs]
+    ignores = ["venv/*", ".git*", "*__pycache__*", "data/*", "*.log", "*.pid"]
+    ignores = [os.path.join(path, dir) for dir in ignores]
 
-    command = ['python', 'main.py']
-    start_monitoring(path, command, ignore_dirs=ignore_dirs)
+    command = ["python", "main.py"]
+    start_monitoring(path, command, ignores=ignores)
