2664672367c86d8908c3d08956bbc6c0a60f93ec
James Wild <wild.james343@gmail.com>
2024-07-07 11:29:22 +0000

    update devcontainer

--- dev_runner.py
+++ dev_runner.py
@@ -15,13 +15,17 @@ basicConfig(level=INFO)
 class ChangeHandler(FileSystemEventHandler):
     """Handles the file change events by restarting the application."""
 
-    def __init__(self, command: List[str], ignores: Optional[List[str]] = None):
+    def __init__(
+        self, command: List[str], ignores: Optional[List[str]] = None
+    ):
         self.command = command
         self.env = os.environ.copy()
         self.process = subprocess.Popen(self.command, env=self.env)
         self.ignores = ignores or []
 
-        logger.info(f"Starting the managed process with command: {self.command}")
+        logger.info(
+            f"Starting the managed process with command: {self.command}"
+        )
         logger.info(f"Ignoring directories: {self.ignores}")
         logger.info(f"Process ID: {self.process.pid}")
 
@@ -49,7 +53,9 @@ class ChangeHandler(FileSystemEventHandler):
         logger.info(f"New Process ID: {self.process.pid}")
 
 
-def start_monitoring(path, command: List[str], ignores: Optional[List[str]] = None):
+def start_monitoring(
+    path, command: List[str], ignores: Optional[List[str]] = None
+):
     event_handler = ChangeHandler(command, ignores)
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
@@ -66,8 +72,19 @@ if __name__ == "__main__":
     # Set the path to the project directory
     path = sys.argv[1] if len(sys.argv) > 1 else "."
 
-    ignores = ["venv/*", ".git*", "*__pycache__*", "data/*", "*.log", "*.pid"]
+    ignores = [
+        "venv/*",
+        ".git*",
+        "*__pycache__*",
+        "data/*",
+        "*.log",
+        "*.pid",
+        "dev_runner.py",
+        ".devcontainer/*",
+    ]
     ignores = [os.path.join(path, dir) for dir in ignores]
+
+    exceptions = ["data/config.yaml"]
 
     command = ["python", "main.py"]
     start_monitoring(path, command, ignores=ignores)
